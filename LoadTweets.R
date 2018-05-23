4d03e0bc93d33da3ba6b933f77477d09179c0d0a
Sam Zipper <samuelczipper@gmail.com>
2018-05-23 15:59:36 -0700

    Update EcohydroStream to rtweet version

--- LoadTweets.R
+++ LoadTweets.R
@@ -16,10 +16,9 @@ require(dplyr)
 
 # output directory: this is where the SQLite database is
 out.dir <- "C:/Users/Sam/Dropbox/Work/Twitter/EcohydroStream/"
-#out.dir <- "D:/Dropbox/Work/Twitter/EcohydroStream/"
 
 # path to database
-path.out <- paste0(out.dir, "TweetsOut.sqlite")
+path.out <- paste0(out.dir, "rTweetsOut.sqlite")
 
 # connect to database
 db <- dbConnect(RSQLite::SQLite(), path.out)
@@ -27,22 +26,36 @@ db <- dbConnect(RSQLite::SQLite(), path.out)
 # read in table
 df <- dbReadTable(db, "tweets")
 
+# trim to unique and rewrite
+df <- unique(df)
+dbWriteTable(db, "tweets", df, overwrite=T)
+
 # when you're done, disconnect from database (this is when the data will be written)
 dbDisconnect(db)
 
 # plot of tweets by day
-df$created <- ymd_hms(df$created)
-df$DOY <- yday(df$created)
-df$Date <- as.Date(df$created)
+df$created_at <- ymd_hms(df$created_at)
+df$DOY <- yday(df$created_at)
+df$Date <- as.Date(df$created_at, tz=Sys.timezone(location = TRUE))
 df.d <- summarize(group_by(df, Date),
-                  tweets = sum(is.finite(Date)))
+                  tweets = sum(is.finite(lat.location)))
+
+# list of missing days
+missing <- seq(df.d$Date[1], Sys.Date()-1, by="day")[!(seq(df.d$Date[1], Sys.Date()-1, by="day") %in% df.d$Date)]
+print(missing)
+#print(yday(missing))
+#print(week(missing))
 
 # print most recent tweet
-print(paste0("Last tweet: ", df$created[which.max(df$id)]))
+print(paste0("Last tweet: ", df$created_at[which.max(df$status_id)]))
 
+# bar plot: tweets by day
 p.bar.tweets.DOY <-
   ggplot(df.d, aes(x=Date, y=tweets)) +
   geom_bar(stat="identity") +
+  geom_hline(yintercept=0, color="gray65") +
+  labs(title=paste0(sum(df.d$tweets), " Ecohydro Tweets")) +
+  scale_y_continuous(name="# Tweets") +
   theme_bw() +
   theme(panel.grid=element_blank())
 p.bar.tweets.DOY
--- SearchAndStoreTweets.R
+++ SearchAndStoreTweets.R
@@ -11,27 +11,30 @@ rm(list=ls())
 git.dir <- "C:/Users/Sam/WorkGits/EcohydroStream/"
 
 # load packages
-require(twitteR)
+require(rtweet)
 require(lubridate)
 require(ggmap)
 require(stringr)
+require(stringi)
 require(maptools)
 require(DBI)
 require(ROAuth)
+require(dplyr)
+require(httpuv)
 
-# search string: what will you search twitter for?
-search.str <- "eco-hydrology OR ecohydrology OR hydroecology OR hydro-ecology OR #hydroecology OR #ecohydrology OR (ecology and (hydrology OR hydrogeology))"
+# get today/yesterday dates
+date_today <- as.Date(Sys.time(), tz=Sys.timezone(location = TRUE))
+date_yesterday <- date_today-days(1)
 
 # output directory: save to Dropbox, not git repository, so it's automatically backed up
 # this is also where authentication info is stored
-#out.dir <- "C:/Users/Sam/Dropbox/Work/Twitter/EcohydroStream/"
-out.dir <- "D:/Dropbox/Work/Twitter/EcohydroStream/"
+out.dir <- "C:/Users/Sam/Dropbox/Work/Twitter/EcohydroStream/"
 
 # path to save output data
-path.out <- paste0(out.dir, "TweetsOut.sqlite")
+path.out <- paste0(out.dir, "rTweetsOut.sqlite")
 
 # path to save the screen output
-path.sink <- paste0(out.dir, "TweetsOut_Screen_", format(Sys.time(), "%Y%m%d-%H%M"), ".txt")
+path.sink <- paste0(out.dir, "rTweetsOut_Screen_", format(Sys.time(), "%Y%m%d-%H%M"), ".txt")
 
 ## launch sink file, which will store screen output 
 # this is useful when automating, so it can be double-checked later
@@ -39,57 +42,54 @@ path.sink <- paste0(out.dir, "TweetsOut_Screen_", format(Sys.time(), "%Y%m%d-%H%
 s <- file(path.sink, open="wt")
 sink(s, type="message")
 
-# relative path to authentication info (this is in .gitignore
-# so not shared publicly). these are obtained from twitter/google
-# when you create your app.
-path.auth.t <- paste0(out.dir, "TwitterAuth.txt")
+# status update
+print(paste0("starting, from ", date_yesterday, " to ", date_today))
 
-# read in authentication info - file has three lines of 
-# comments followed by:
-#   [1] consumer key (API key)
-#   [2] consumer secret (API secret)
-#   [3] access token
-#   [4] access secret
-auth.t <- read.table(path.auth.t, skip=3, stringsAsFactors=F)[,1]    # read in as vector
+# load existing tweet SQLite database
+db <- dbConnect(RSQLite::SQLite(), path.out)
+df.in <- dbReadTable(db, "tweets")
 
-# set up authentication
-options(httr_oauth_cache=T)   # this will store authentication as a local file
-setup_twitter_oauth(auth.t[1], auth.t[2], auth.t[3], auth.t[4])
-
-# get today/yesterday dates
-date_today <- as.Date(Sys.time())
-date_yesterday <- date_today-days(1)
+# read in token which was created with script rtweet_SetUpToken.R
+r.token <- readRDS(file.path(path.expand("~/"), "twitter_token_ecohydro.Rds"))
 
 # search twitter!
-tweets <- searchTwitter(search.str, 
-                        n=10000, 
-                        resultType="recent",
-                        since=as.character(date_yesterday),
-                        until=as.character(date_today),
-                        retryOnRateLimit=5000)
+tweets <- search_tweets2(search.str,
+                         n=10000, 
+                         type="recent",
+                         include_rts=F,
+                         retryOnRateLimit=T,
+                         token=r.token)
 
-# get rid of retweets
-tweets <- strip_retweets(tweets, strip_manual=T, strip_mt=T)
+# subset to yesterday only, just in case...
+df <- subset(tweets, created_at >= date_yesterday & created_at < date_today)
 
-# put into data frame (only categories we care about)
-df <- twListToDF(tweets)[,c("text", "created", "id", "screenName", "isRetweet", "longitude", "latitude")]
+# get rid of duplicates just in case
+df <- unique(df)
 
-# convert text to UTF-8 to deal with weird characters
-df$text <- sapply(df$text, function(row) iconv(row, to='UTF-8'))
-df$text <- gsub("\n", " ", df$text)
+# tweets status
+print(paste0(dim(df)[1], " tweets"))
 
 ## using Google Maps API, get estimated geographic coordinates based on user location
 # limit of 2500/day! so, get clean location as much as possible first to minimize calls to API
 
 # get user location
-userInfo <- lookupUsers(df$screenName)
-df.users <- twListToDF(userInfo)
+users.unique <- unique(df$screen_name)
+print(paste0(length(users.unique), " screen names"))
+df.users <- lookup_users(users.unique, 
+                         token=r.token)
+
+# users status
+print(paste0(dim(df.users)[1], " users"))
 
 # trim to only users with location info
 df.users <- df.users[df.users$location != "",]
 
-# replace % in user location with blank so geocode doesn't get messed up
+# replace % and # in user location with blank so geocode doesn't get messed up
 df.users$location <- gsub("%", " ",df.users$location)
+df.users$location <- gsub("#", " ",df.users$location)
+
+# deal with emojis and other weird characters
+df.users$location <- iconv(df.users$location, "UTF-8", "ASCII", sub="")
 
 # trim leading/trailing white space
 df.users$location <- trimws(df.users$location)
@@ -97,96 +97,110 @@ df.users$location <- trimws(df.users$location)
 # get unique locations
 locations <- unique(df.users$location)
 
-# call geocode
-geo.out <- geocode(locations, source="google", output="all")
+# get rid of any locations that are empty
+locations <- trimws(locations)
+locations <- locations[!is.na(locations)]
+locations <- locations[!is.null(locations)]
+locations <- locations[!stri_isempty(locations)]
 
-## filter output
-if (length(locations)>1){
-  # status check: did geocode find a location?
-  check.status <- sapply(geo.out, function(x) x["status"]=="OK" & length(x["status"])>0)
-  check.status[is.na(check.status)] <- F
-  geo.out <- geo.out[check.status]
-  locations <- locations[check.status]
-  
-  # status check: is location ambiguous?
-  check.ambig <- sapply(lapply(geo.out, lapply, length), function(x) x["results"]=="1")
-  geo.out <- geo.out[check.ambig]
-  locations <- locations[check.ambig]
-  
-  ## make final locations data frame
-  df.locations <- data.frame(
-    location = locations,
-    lat.location = sapply(geo.out, function(x) x["results"]$results[[1]]$geometry$location$lat),
-    lon.location = sapply(geo.out, function(x) x["results"]$results[[1]]$geometry$location$lng),
-    formatted_address = sapply(geo.out, function(x) x["results"]$results[[1]]$formatted_address)
-  )
-  
-} else {
-  # status check: did geocode find a location?
-  check.status <- geo.out[[2]]=="OK"
-  check.status[is.na(check.status)] <- F
-  if (!check.status){
-    geo.out <- NULL
-    locations <- NULL
+# figure out which locations have already been geocoded
+locations.exist <- locations[str_to_lower(locations) %in% str_to_lower(df.in$location)]
+df.locations.exist <- 
+  data.frame(location = locations.exist,
+             lat.location = df.in$lat.location[match(str_to_lower(locations.exist), str_to_lower(df.in$location))],
+             lon.location = df.in$lon.location[match(str_to_lower(locations.exist), str_to_lower(df.in$location))])
+locations <- locations[!(locations %in% locations.exist)]
+
+# status update
+print(paste0(length(locations), " locations to geocode"))
+
+# make vector to hold empty locations
+lat.location <- rep(NaN, length(locations))
+lon.location <- rep(NaN, length(locations))
+success <- rep(F, length(locations))
+
+# call geocode for each location
+maxtries <- 5
+for (l in 1:length(locations)){
+  check.geocode <- F
+  check.status <- F
+  tries <- 0
+  while (!check.geocode & tries <= maxtries){
+    # count number of tries
+    tries <- tries + 1
+    
+    # geocode
+    l.geo <- ggmap::geocode(locations[l], source="google", output="all")
+    
+    # check if success
+    if (l.geo$status != "OVER_QUERY_LIMIT") check.geocode <- T
+    if (l.geo$status == "OK") check.status <- T
   }
   
-  # status check: is location ambiguous?
-  check.ambig <- lapply(geo.out, lapply, length)$results>=1
-  if (!check.ambig){
-    geo.out <- NULL
-    locations <- NULL
+  if (check.status){
+    # check if location not ambiguous
+    check.ambig <- if (length(l.geo$results)==1) T else F
+    
+    # check if location resolved to state level
+    #   acceptable google address component codes, from https://developers.google.com/maps/documentation/geocoding/intro
+    add.comp.state <- c("locality", "postal_code", "neighborhood", "park", "sublocality", "locality",
+                        paste0("administrative_area_level_", seq(1,5)))
+    add.comps <- unlist(l.geo$results[[1]]$address_components)
+    if (sum(add.comps[which(names(add.comps)=="types1")] %in% add.comp.state) > 0){
+      check.state <- T
+    } else {
+      check.state <- F
+    }
+    
+    # figure out: is this a good geocode?
+    if (check.status & check.ambig & check.state){
+      success[l] <- T
+      lat.location[l] <- l.geo$results[[1]]$geometry$location$lat
+      lon.location[l] <- l.geo$results[[1]]$geometry$location$lng
+    }
+    
   }
   
-  ## make final locations data frame
-  df.locations <- data.frame(
-    location = locations,
-    lat.location = geo.out$results[[1]]$geometry$location$lat,
-    lon.location = geo.out$results[[1]]$geometry$location$lng,
-    formatted_address = geo.out$results[[1]]$formatted_address
-  )
 }
 
+## make final locations data frame
+df.locations <- rbind(df.locations.exist,
+                      data.frame(
+                        location = locations[success],
+                        lat.location = lat.location[success],
+                        lon.location = lon.location[success]
+                      ))
+
+# status update
+print(paste0(length(locations), " locations successfully geocoded"))
+
 # add location info back to user data frame
-df.users <- merge(df.users[c("location", "description", "screenName")], df.locations, by="location", all.x=T)
+df.users <- left_join(df.users[c("location", "description", "screen_name")], df.locations, by="location", all.x=T)
 
 # make output data frame including tweet, user, location, etc.
-df.out <- merge(df, df.users, by="screenName", all.x=T)
-
-# get rid of commas
-df.out$text <- gsub(",", " ", df.out$text)
-df.out$location <- gsub(",", " ", df.out$location)
-df.out$description <- gsub(",", " ", df.out$description)
-
-# get rid of line breaks
-df.out$text <- gsub("\n", " ", df.out$text)
-df.out$location <- gsub("\n", " ", df.out$location)
-df.out$description <- gsub("\n", " ", df.out$description)
-
-# get rid of URLs
-removeURL <- function(x) gsub("http[[:alnum:][:punct:]]*", "", x)
-df.out$text <- unlist(lapply(df.out$text, removeURL))
-
-# trim leading/trailing white space
-df.out$text <- trimws(df.out$text)
-
-# get rid of duplicate tweets
-df.out <- df.out[!duplicated(df.out[c("screenName", "text")]), ]
+df.out <- left_join(df, df.users, by="screen_name", all.x=T)
 
 # put in order
-df.out <- df.out[order(df.out$id), ]
+df.out <- df.out[order(df.out$status_id), ]
 
 # convert dates to character string for database
-df.out$created <- as.character(df.out$created)
+df.out$created_at <- as.character(df.out$created_at)
+
+## convert columns that are lists to text strings separated by _<>_
+# find list columns
+cols.list <- which(lapply(df.out, class) == "list")
+
+for (col in cols.list){
+  df.out[,col] <- apply(df.out[,col], 1, function(x) as.character(paste(x, collapse="_<>_")))
+}
 
 ## put into database
-# create/connect to database
-db <- dbConnect(RSQLite::SQLite(), path.out)
-
 # add data frame to database (if it doesn't exist, it will be created)
 dbWriteTable(db, "tweets", df.out, append=T)
 
 # if you want to read in a data frame from your db to check...
 #df.test <- dbReadTable(db, "tweets")
+#dbWriteTable(db, "tweets", df.test, overwrite=T)
 
 # when you're done, disconnect from database (this is when the data will be written)
 dbDisconnect(db)
@@ -196,16 +210,3 @@ print(paste0(dim(df.out)[1], " tweets added to database"))
 
 # close sink
 close(s)
-sink()
-sink(type="message")
-close(s)
-
-# # make a plot
-# state_map <- map_data("state")
-# p.map <-
-#   ggplot(data=df.out, aes(x=lon.location, y=lat.location)) +
-#   geom_path(data=state_map, color="blue", aes(x=long, y=lat, group=factor(region))) +
-#   geom_point(shape=21) +
-#   coord_map() +
-#   theme_bw() +
-#   theme(panel.grid=element_blank())
